e5c3499e8fd3a66a0a7538f4b1d8851ffb0f3d96
remzitepe <remzi.tepe@ug.bilkent.edu.tr>
2022-04-24 22:22:07 +0300

    my account db changes

---
+++ change_info.php
@@ -0,0 +1,32 @@
+<?php
+   include("config.php");
+   session_start();
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      $username_session = $_SESSION['login_user'];
+      $username = $_POST["username"]; 
+      $password = $_POST["password"]; 
+      $cpassword = $_POST["password-check"];
+      $firstName = $_POST["firstName"];
+      $lastName = $_POST["lastName"];
+      $identificationNum = $_POST["identification-num"];
+      $birthdayDate = $_POST["birthdayDate"];
+      $emailAddress = $_POST["emailAddress"];
+      $phoneNumber = $_POST["phoneNumber"];
+      
+      if( $password === $cpassword){
+        $sql = "UPDATE person SET username = '$username', first_name = '$firstName', last_name = '$lastName', identification_num = '$identificationNum', birthday = '$birthdayDate', email = '$emailAddress', phone_num = '$phoneNumber', password = '$password' WHERE username = '$username_session'";
+        $result = mysqli_query($db,$sql);
+        
+              
+              $_SESSION['login_user'] = $username;
+              $_SESSION['login_pass'] = $password;
+              $_SESSION['login_flag'] = true;
+              header("location: profile.php");
+        }
+        else { 
+            $showError = "Passwords do not match"; 
+        }  
+    }
+    
+?>
--- profile.php
+++ profile.php
@@ -16,8 +16,21 @@
       $email = $row['email'];
       $phone_num = $row['phone_num'];
       $password = $row['password'];
+      $password_check = $password;
   }
+  if ( isset($_POST['disable'])){
+    $disable = 'enabled';
+    $display_edit = 'display: none';
+    $display_submit = 'display: block';
+    $disable_pass = 'display: block';
+  }
+  else{
+    $disable = 'disabled';
+    $display_edit = 'display: block';
+    $disable_pass = 'display: none';
+    $display_submit = 'display: none';
 
+  }
 ?>
 <!doctype html>
 <html lang="en">
@@ -52,15 +65,14 @@
         <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
           <div class="card-body p-4 p-md-5">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">My Profile</h3>
-            <form>
+            <form action='change_info.php' method='post'>
               <div class="row">
                 <div class="col-md-6 mb-4">
 
                   <div class="form-outline">
                     <div class="row">
-                    <input type="text" id="firstName" class="form-control form-control-lg" value='<?php echo $first_name ?>'disabled/>
+                    <input type="text" id="firstName" name="firstName" class="form-control form-control-lg" value='<?php echo $first_name ?>' <?php echo $disable ?>/>
                     <label class="form-label" for="firstName">First Name</label>
-                    <button onclick='activate_first_name()'>edit</button>
                     </div>
                   </div>
                 </div>
@@ -68,7 +80,7 @@
                 <div class="col-md-6 mb-4">
 
                   <div class="form-outline">
-                    <input type="text" id="lastName" class="form-control form-control-lg" value='<?php echo $last_name ?>' disabled />
+                    <input type="text" id="lastName" name="lastName" class="form-control form-control-lg" value='<?php echo $last_name ?>' <?php echo $disable ?> />
                     <label class="form-label" for="lastName">Last Name</label>
                   </div>
 
@@ -79,7 +91,7 @@
                 <div class="col-md-6 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="text" id="username" class="form-control form-control-lg" value='<?php echo $username ?>' disabled />
+                    <input type="text" id="username" name="username" class="form-control form-control-lg" value='<?php echo $username ?>' <?php echo $disable?> />
                     <label class="form-label" for="username">Username</label>
                   </div>
 
@@ -87,7 +99,7 @@
                 <div class="col-md-6 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="text" id="identification-num" class="form-control form-control-lg" value='<?php echo $identification_num ?>' disabled  />
+                    <input type="text" id="identification-num" name="identification-num"class="form-control form-control-lg" value='<?php echo $identification_num ?>' <?php echo $disable ?>  />
                     <label class="form-label" for="identification-num">TC identification Number</label>
                   </div>
 
@@ -100,7 +112,7 @@
                     <input
                       type="date"
                       class="form-control form-control-lg"
-                      id="birthdayDate" value='<?php echo $birthday ?>' disabled 
+                      id="birthdayDate" name="birthdayDate" value='<?php echo $birthday ?>' <?php echo $disable ?> 
                     />
                     <label for="birthdayDate" class="form-label">Birthday</label>
                   </div>
@@ -112,7 +124,7 @@
                 <div class="col-md-6 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="email" id="emailAddress" class="form-control form-control-lg"  value='<?php echo $email ?>' disabled />
+                    <input type="email" id="emailAddress" name="emailAddress" class="form-control form-control-lg"  value='<?php echo $email ?>' <?php echo $disable ?> />
                     <label class="form-label" for="emailAddress">Email</label>
                   </div>
 
@@ -120,7 +132,7 @@
                 <div class="col-md-6 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="tel" id="phoneNumber" class="form-control form-control-lg" value='<?php echo $phone_num ?>' disabled  />
+                    <input type="tel" id="phoneNumber" name="phoneNumber" class="form-control form-control-lg" value='<?php echo $phone_num ?>' <?php echo $disable ?>  />
                     <label class="form-label" for="phoneNumber">Phone Number</label>
                   </div>
 
@@ -131,14 +143,26 @@
                 <div class="col-md-6 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="password" id="password" class="form-control form-control-lg"   value='<?php echo $password ?>' disabled   />
+                    <input type="password" id="password" name="password" class="form-control form-control-lg" value='<?php echo $password ?>' <?php echo $disable ?>   />
                     <label class="form-label" for="password">Password</label>
                   </div>
 
                 </div>
-              </div>
+                <div class="col-md-6 mb-4 pb-2">
 
+                  <div class="form-outline" style='<?php echo $disable_pass ?>'>
+                    <input type="password" id="password-check" name="password-check" class="form-control form-control-lg" value ='<?php echo $password_check ?>'  <?php echo $disable ?>/>
+                    <label class="form-label" for="password-check">Confirm Password</label>
+                  </div>
+
+                  </div>
+                  <input class="btn btn-primary" type="submit" name='submit-change' value="Change" style='<?php echo $display_submit ?>'>
+
+              </div>
             </form>
+            <form method='post'>
+                    <input class="btn btn-primary" type="submit" name='disable' value="Edit" style='<?php echo $display_edit ?>'>
+                    </form>
           </div>
         </div>
       </div>
