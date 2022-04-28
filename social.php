7846e8ca4a26c41dbde0421c3711632708dea801
DenizOzal <semih.ozal@bilkent.edu.tr.com>
2022-04-29 02:34:25 +0300

    social.php is created

--- social.php
+++ social.php
@@ -91,232 +91,148 @@
  </nav>
 
  <main>
- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container bootstrap snippets bootdey">
-<div class="col-md-8">
-  <div class="box box-widget">
-    <div class="box-header with-border">
-      <div class="user-block">
-        <img class="img-circle" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="User Image">
-        <span class="username"><a href="#">Jonathan Burke Jr.</a></span>
-        <span class="description">Shared publicly - 7:30 PM Today</span>
-      </div>
-      <div class="box-tools">
-      <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="" data-original-title="Mark as read">
-        <i class="fa fa-circle-o"></i></button>
-        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-      </div>
+    <div class="row" style="margin-right:36px;">
+            <div class="col-10 d-flex justify-content-end">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                       Sort By
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="old-to-new.php">Old to New</a>
+                        <a class="dropdown-item" href="new-to-old.php">New to Old</a>
+                        <a class="dropdown-item" href="most-likes.php">Most Likes</a>
+                        <a class="dropdown-item" href="most-comments.php">Most Comments</a>
+                    </div>
+                </div>
+            </div>
     </div>
-    <div class="box-body">
-      <p>Far far away, behind the word mountains, far from the
-      countries Vokalia and Consonantia, there live the blind
-      texts. Separated they live in Bookmarksgrove right at</p>
+     <div class="container-fluid" style="border-style:solid; width:60%;">
+        <div class="row">
+            <div class="col-2">
+            <h5>@victusmaneo</h5>
+            </div>
+            <div class="col-5">
+            <h5>(146 followers)</h5>
+            </div>
+            <div class="col-1.5">
+            <h5>Shared at</h5>
+            </div>
+            <div class="col-1.5">
+            <h5>: 29 April 2022 - 21.30</h5>
+            </div>
+            <div class="col-12">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, magni necessitatibus repellendus alias at quod dicta sapiente adipisci nesciunt labore fugiat voluptate quo in deserunt aliquid provident explicabo? Molestias, sapiente.</p>
+            </div>
+        </div>
 
-      <p>the coast of the Semantics, a large language ocean.
-      A small river named Duden flows by their place and supplies
-      it with the necessary regelialia. It is a paradisematic
-      country, in which roasted parts of sentences fly into
-      your mouth.</p>
-
-      <div class="attachment-block clearfix">
-        <img class="attachment-img" src="https://via.placeholder.com/400x300/" alt="Attachment Image">
-        <div class="attachment-pushed">
-        <h4 class="attachment-heading"><a href="http://www.lipsum.com/">Lorem ipsum text generator</a></h4>
-        <div class="attachment-text">
-        Description about the attachment can be placed here.
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry... <a href="#">more</a>
+        <div class="row">
+            <div class="col-10">
+                <div id="accordion">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                        <h5 class="mb-0">
+                            <button class="btn btn-lg  btn-block" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            2 match | Odd: 4.80 | Coupon Price: 3 $
+                            <span style="float:right;"> 156 people played </span>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body">
+                            <table class="table table-dark">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">MBN</th>
+                                <th scope="col">Match</th>
+                                <th scope="col">Time/Date </th>
+                                <th scope="col">Odd Type / Odd</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>1</td>
+                                <td>Liverpool - Manchester City</td>
+                                <td>19.30 / 4.29.2022</td>
+                                <td>MR1: 1.17 </td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>1</td>
+                                <td>Everton - Chealsea</td>
+                                <td>20.00 / 4.29.2022</td>
+                                <td>MG+: 2.50 </td>
+                                </tr>
+                                <tr>
+                            </tbody>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <button type="button" style="margin-top:15px;" class="btn btn-primary">Play Betslip</button>
+            </div>
         </div>
+        <div class="row">
+            <div class="col-4">
+                <button type="button" style="margin-top:5px;" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14.854 4.854a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 4H3.5A2.5 2.5 0 0 0 1 6.5v8a.5.5 0 0 0 1 0v-8A1.5 1.5 0 0 1 3.5 5h9.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4z"/>
+                    </svg>
+                    Share
+                </button>
+                <button type="button" style="margin-top:5px;" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                    Like
+                </button>
+            </div>
+            <div class="col-8 d-flex justify-content-end" style="margin-top:5px;">
+                <h5>45 Likes - <span data-toggle="collapse" href="#collapseComment" role="button" aria-expanded="false" id="comment-link">2 comments </span></h5>
+            </div>
         </div>
-      </div>
-      <button type="button" class="btn btn-default btn-xs"><i class="fa fa-share"></i> Share</button>
-      <button type="button" class="btn btn-default btn-xs"><i class="fa fa-thumbs-o-up"></i> Like</button>
-      <span class="pull-right text-muted">45 likes - 2 comments</span>
+        <div class="row">
+            <div class="col-12">
+                <div class="collapse" id="collapseComment">
+                    <div class="card card-body">
+                        <h5 class="card-title">Arman Engin Sucu <span style="float:right;">20.03 - 29/04/2022</span></h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo laborum libero perspiciatis animi quibusdam iure quas ex dolore recusandae beatae. Minus, illum quibusdam aut inventore qui magnam aliquam voluptatum numquam?</p>
+                    </div>
+                    <div class="card card-body">
+                        <h5 class="card-title">Remzi Tepe <span style="float:right;">20.03 - 29/04/2022</span></h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo laborum libero perspiciatis animi quibusdam iure quas ex dolore recusandae beatae. Minus, illum quibusdam aut inventore qui magnam aliquam voluptatum numquam?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-10">
+                <div class="form-group">
+                    <label for="Comment">Comment</label>
+                    <textarea class="form-control" id="comment-id" rows="2"></textarea>
+                </div>
+            </div>
+            <div class="col-2">
+                <button type="button" class="btn btn-primary btn-lg btn-block" style="margin-top:40px;">Apply</button>
+            </div>
+        </div>
     </div>
-    <div class="box-footer box-comments">
-      <div class="box-comment">
-        <img class="img-circle img-sm" src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="User Image">
-        <div class="comment-text">
-          <span class="username">
-          Maria Gonzales
-          <span class="text-muted pull-right">8:03 PM Today</span>
-          </span>
-          It is a long established fact that a reader will be distracted
-          by the readable content of a page when looking at its layout.
-        </div>
-      </div>
-      <div class="box-comment">
-        <img class="img-circle img-sm" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="User Image">
-        <div class="comment-text">
-          <span class="username">
-          Nora Havisham
-          <span class="text-muted pull-right">8:03 PM Today</span>
-          </span>
-          The point of using Lorem Ipsum is that it has a more-or-less
-          normal distribution of letters, as opposed to using
-          'Content here, content here', making it look like readable English.
-        </div>
-      </div>
-    </div>
-    <div class="box-footer">
-      <form action="#" method="post">
-        <img class="img-responsive img-circle img-sm" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="Alt Text">
-        <div class="img-push">
-          <input type="text" class="form-control input-sm" placeholder="Press enter to post comment">
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-</div>
  </main>
 </html>
 
 <style>
     body{
-    margin-top:20px;
-    background-color: #ecf0f5;
-}
-.box-widget {
-    border: none;
-    position: relative;
-}
-.box {
-    position: relative;
-    border-radius: 3px;
-    background: #ffffff;
-    border-top: 3px solid #d2d6de;
-    margin-bottom: 20px;
-    width: 100%;
-    box-shadow: 0 1px 1px rgba(0,0,0,0.1);
-}
-.box-header.with-border {
-    border-bottom: 1px solid #f4f4f4;
-}
-.box-header {
-    color: #444;
-    display: block;
-    padding: 10px;
-    position: relative;
-}
-.user-block img {
-    width: 40px;
-    height: 40px;
-    float: left;
-}
-.user-block .username {
-    font-size: 16px;
-    font-weight: 600;
-}
-.user-block .description {
-    color: #999;
-    font-size: 13px;
-}
-.user-block .username, 
-.user-block .description, 
-.user-block .comment {
-    display: block;
-    margin-left: 50px;
-}
-.box-header>.box-tools {
-    position: absolute;
-    right: 10px;
-    top: 5px;
-}
-.btn-box-tool {
-    padding: 5px;
-    font-size: 12px;
-    background: transparent;
-    color: #97a0b3;
-}
-.box-body {
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 3px;
-    border-bottom-left-radius: 3px;
-    padding: 10px;
-}
-.pad {
-    padding: 10px;
-}
-.box .btn-default {
-    background-color: #f4f4f4;
-    color: #444;
-    border-color: #ddd;
-}
-.box-comments {
-    background: #f7f7f7 !important;
-}
-.box-footer {
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 3px;
-    border-bottom-left-radius: 3px;
-    border-top: 1px solid #f4f4f4;
-    padding: 10px;
-    background-color: #fff;
-}
-.box-comments .box-comment:first-of-type {
-    padding-top: 0;
-}
-.box-comments .box-comment {
-    padding: 8px 0;
-    border-bottom: 1px solid #eee;
-}
-.img-sm, 
-.box-comments .box-comment img, 
-.user-block.user-block-sm img {
-    width: 30px !important;
-    height: 30px !important;
-}
-.img-sm, .img-md, 
-.img-lg, .box-comments .box-comment img, 
-.user-block.user-block-sm img {
-    float: left;
-}
-.box-comments .comment-text {
-    margin-left: 40px;
-    color: #555;
-}
-.box-comments .username {
-    color: #444;
-    display: block;
-    font-weight: 600;
-}
-.box-comments .text-muted {
-    font-weight: 400;
-    font-size: 12px;
-}
-.img-sm+.img-push {
-    margin-left: 40px;
-}
-.box .form-control {
-    border-radius: 0;
-    box-shadow: none;
-    border-color: #d2d6de;
-}
-.attachment-block {
-    border: 1px solid #f4f4f4;
-    padding: 5px;
-    margin-bottom: 10px;
-    background: #f7f7f7;
-}
-.attachment-block .attachment-img {
-    max-width: 100px;
-    max-height: 100px;
-    height: auto;
-    float: left;
-}
-.attachment-block .attachment-pushed {
-    margin-left: 110px;
-}
-.attachment-block .attachment-heading {
-    margin: 0;
-}
-.attachment-block .attachment-heading .h4, .attachment-block .attachment-heading h4 {
-    font-size: 18px;
-}
-.attachment-block .attachment-text {
-    color: #555;
-}
+        background-color: #ecf0f5;
+    }
+    .hr{
+        border: 1px solid red;
+    }
+    #comment-link:hover{
+        color:white;
+    }
+    
 </style>
