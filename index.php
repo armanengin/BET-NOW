88d71a7c3e201fc793a99f5adacd54ffe6f81f18
DenizOzal <semih.ozal@bilkent.edu.tr.com>
2022-04-29 17:24:23 +0300

    friends.php is created

--- index.php
+++ index.php
@@ -13,9 +13,6 @@
   else{
     $display_acc_div = 'display: none';
   }
-  $div = "<div>
-  denizke
-</div>";
 ?>
 <!doctype html>
 <html lang="en">
@@ -41,61 +38,61 @@
   </body>
 
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-    <a class="navbar-brand" href="/BET-NOW/index.phpindex.php">
-    <img src="assets/b-n.jpeg" width="30" class="d-inline-block align-top" alt="logo" class="img-fluid">
-    <span style="font-family:Times New Roman; color:ForestGreen;">
-    Bet-Now
-    </span>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+      <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+      <a class="navbar-brand" href="/BET-NOW/index.phpindex.php">
+      <img src="assets/b-n.jpeg" width="30" class="d-inline-block align-top" alt="logo" class="img-fluid">
+      <span style="font-family:Times New Roman; color:ForestGreen;">
+      Bet-Now
+      </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="index.php" style="font-family:Times New Roman;">LIVE <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="football.php">Football</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="volleyball.php">Volleyball</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="lottery.php">Lottery</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="horce-race.php">Horse Race</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="editors.php">Editors</a>
-        </li>
-        </ul>
-        <div class="login-container" name="login-box" id="login-box" style='<?php echo $show_log ?>'>
-            <form action="login.php" method="post">
-            <input type="text" placeholder="Username" name="username">
-            <input type="password" placeholder="Password" name="password">
-            <button class="btn btn-primary btn-sm" type="submit">Login</button>
-            <button class="btn btn-primary btn-sm" onclick="location.href='signup.php'" type="button">Signup</button>
-            </form>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+              <a class="nav-link" href="index.php" style="font-family:Times New Roman;">LIVE <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="football.php">Football</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="volleyball.php">Volleyball</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="lottery.php">Lottery</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="horce-race.php">Horse Race</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="editors.php">Editors</a>
+          </li>
+          </ul>
+          <div class="login-container" name="login-box" id="login-box" style='<?php echo $show_log ?>'>
+              <form action="login.php" method="post">
+              <input type="text" placeholder="Username" name="username">
+              <input type="password" placeholder="Password" name="password">
+              <button class="btn btn-primary btn-sm" type="submit">Login</button>
+              <button class="btn btn-primary btn-sm" onclick="location.href='signup.php'" type="button">Signup</button>
+              </form>
+          </div>
+          <div class="user-info" name="account-box" id="user-info" style='<?php echo $show_acc ?>'>
+          <a class="btn btn-success" href="friends.php" role="button">Friends</a>
+          <div class='col' style='<?php echo $display_acc_div ?>'>
+            <a class="btn btn-info" href="profile.php" role="button">My Profile</a>
+            <a class="btn btn-info" href="deposit_credit.php" role="button">Deposit Credit</a>
+            <a class="btn btn-info" href="withdraw_credit.php" role="button">Withdraw Credit</a>
+            <a class="btn btn-danger" href="logout.php" role="button">Logout</a>
+          </div>
+          <form method='post'>
+            <input class="btn btn-primary" type="submit" name="acc-box" value="My Account">
+        </form>
         </div>
-        <div class="user-info" name="account-box" id="user-info" style='<?php echo $show_acc ?>'>
-        <a class="btn btn-success" href="friends.php" role="button">Friends</a>
-        <div class='col' style='<?php echo $display_acc_div ?>'>
-          <a class="btn btn-info" href="profile.php" role="button">My Profile</a>
-          <a class="btn btn-info" href="deposit_credit.php" role="button">Deposit Credit</a>
-          <a class="btn btn-info" href="withdraw_credit.php" role="button">Withdraw Credit</a>
-          <a class="btn btn-danger" href="logout.php" role="button">Logout</a>
-        </div>
-        <form method='post'>
-          <input class="btn btn-primary" type="submit" name="acc-box" value="My Account">
-      </form>
-      </div>
-    </nav>
-
-
+      </nav>
+    <?php 
+    ?>
 <header>
   <!-- Sidebar -->
   <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
